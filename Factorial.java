a10763fcadfa0eb84f7e6aa777c91fde6453a375
Lena Sakhankova <100844312+LenaSakhankova@users.noreply.github.com>
2023-03-22 15:14:37 +0300

    task 1
    adress to namber
    number to adress

---
+++ Factorial.java
@@ -0,0 +1,35 @@
+public class Factorial{
+
+    public static void main(String[] args) {
+        int n = 66; // Indef if n > 66
+        un(n);
+    }
+
+    public static void un(int n) throws ExceptionInInitializerError
+    {
+        try {
+            if(n<1 || n>=66) throw new ExceptionInInitializerError();
+            double un = (1 / factorial(n)) * sumFactorials(n);
+            System.out.format("%.6f", un);
+        }
+        catch (ExceptionInInitializerError ex){
+            System.out.println("n should be between 1 and 65");
+        }
+    }
+
+    public static double factorial(int n) {
+        long temp = 1;
+        for (int i = 1; i <= n; i++) {
+            temp *= i;
+        }
+        return temp;
+    }
+
+    public static double sumFactorials(int n) {
+        long temp = 0;
+        for (int i = 1; i <= n; i++) {
+            temp += factorial(i);
+        }
+        return temp;
+    }
+}
